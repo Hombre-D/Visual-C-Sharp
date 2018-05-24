d8032b68ea4e1e7da5108773369c192c5d013830
Danjel Nygard <danjelnygard@hotmail.com>
2018-05-23 21:55:29 -0500

    First Commit

---
+++ Chapter 4/Exercise 4-10/Invoice/Invoice/Invoice.cs
@@ -0,0 +1,56 @@
+﻿//Exercise 4.10, Invoice class
+
+class Invoice
+{
+    public string PartNumber { get; set; }
+    public string Description { get; set; }
+    private int quantity;
+    private decimal pricePerItem;
+
+    public Invoice (string partNumber, string description, int quantity, decimal pricePerItem)
+    {
+        PartNumber = partNumber;
+        Description = description;
+        Quantity = quantity;
+        PricePerItem = pricePerItem;
+    }
+
+    public int Quantity
+    {
+        get
+        {
+            return quantity;
+        }
+
+        set
+        {
+            if (value > 0)
+            {
+                quantity = value;
+            }
+        }
+    }
+
+    public decimal PricePerItem
+    {
+        get
+        {
+            return pricePerItem;
+        }
+
+        set
+        {
+            if (value > 0.0m)
+            {
+                pricePerItem = value;
+            }
+            
+        }
+    }
+
+    public decimal GetInvoiceAmount()
+    {
+        return quantity * pricePerItem;
+    }
+
+}
---
+++ Chapter 4/Exercise 4-10/Invoice/Invoice/InvoiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class InvoiceTest
+{
+    static void Main(string[] args)
+    {
+        Invoice invoice1 = new Invoice("12", "Order 1", 4, 5.99m);
+        Console.WriteLine($"Part Number: {invoice1.PartNumber}");
+        Console.WriteLine($"Description: {invoice1.Description}");
+        Console.WriteLine($"Quantity: {invoice1.Quantity}");
+        Console.WriteLine($"Price Per Item: {invoice1.PricePerItem}\n");
+
+        Console.Write("Enter a new quantity: ");
+        invoice1.Quantity = int.Parse(Console.ReadLine());
+        Console.WriteLine($"New Quantity: {invoice1.Quantity}\n");
+        Console.Write("Enter a new Price Per Item: ");
+        invoice1.PricePerItem = decimal.Parse(Console.ReadLine());
+        Console.WriteLine($"New Price Per Item: {invoice1.PricePerItem}\n");
+        Console.WriteLine($"Invoice Amount: {invoice1.GetInvoiceAmount()}");
+
+    }
+ }
+
