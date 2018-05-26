057a07355250b36c68573eb2336fb6fe8e238c24
Danjel Nygard <danjelnygard@hotmail.com>
2018-05-25 21:44:41 -0500

    Added Exercise 4-11

---
+++ Chapter 4/Exercise 4-11/Employee/Employee/Employee.cs
@@ -0,0 +1,43 @@
+﻿//Exercise 4-11. Employee Class
+
+class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    private decimal monthlySalary;
+
+    public Employee(string firstName, string lastName, decimal monthlySalary)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        MonthlySalary = monthlySalary;
+    }
+
+    public decimal MonthlySalary
+    {
+        get
+        {
+            return monthlySalary;
+        }
+
+        set
+        {
+            if(value > 0.0m)
+            {
+                monthlySalary = value;
+            }
+        }
+    }
+
+    public decimal YearSalary()
+    {
+        return monthlySalary * 12;
+    }
+
+    public void GiveRaise(decimal raise)
+    {
+        monthlySalary += monthlySalary * raise;
+    }
+
+}
---
+++ Chapter 4/Exercise 4-11/Employee/Employee/EmployeeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class EmployeeTest
+{
+    static void Main(string[] args)
+    {
+        Employee employee1 = new Employee("Dan", "Nygard", 1000.0m);
+        Employee employee2 = new Employee("Nicole", "Nyygard", 2000.0m);
+        Console.WriteLine($"Employee 1: {employee1.FirstName} {employee1.LastName}, " +
+            $"Monthly Salary: {employee1.MonthlySalary} Yearly Salary: {employee1.YearSalary()}");
+        Console.WriteLine($"Employee 2: {employee2.FirstName} {employee2.LastName}, " +
+            $"Monthly Salary: {employee2.MonthlySalary} Yearly Salary: {employee2.YearSalary()}");
+        employee1.GiveRaise(.1m);
+        employee2.GiveRaise(.1m);
+        Console.WriteLine("After Raise:");
+        Console.WriteLine($"Employee 1: {employee1.FirstName} {employee1.LastName}, " +
+            $"Monthly Salary: {employee1.MonthlySalary} Yearly Salary: {employee1.YearSalary()}");
+        Console.WriteLine($"Employee 2: {employee2.FirstName} {employee2.LastName}, " +
+            $"Monthly Salary: {employee2.MonthlySalary} Yearly Salary: {employee2.YearSalary()}");
+    }
+}
+
