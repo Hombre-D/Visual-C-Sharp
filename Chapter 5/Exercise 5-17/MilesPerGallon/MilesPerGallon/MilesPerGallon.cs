ce35cf2005d11fadfb6cb985c7d038622110d2d7
Danjel Nygard <danjelnygard@hotmail.com>
2018-05-28 21:37:07 -0500

    Added Exercise 5-17

---
+++ Chapter 5/Exercise 5-17/MilesPerGallon/MilesPerGallon/MilesPerGallon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+class MilesPerGallon
+{
+    static void Main(string[] args)
+    {
+        int gallons;
+        int miles;
+        double mpg;
+        int totalGallons = 0;
+        int totalMiles = 0;
+        double totalMPG;
+
+        Console.Write("Enter gallons in this tankful (or number < 1 to quit): ");
+        gallons = int.Parse(Console.ReadLine());
+        while (gallons > 0)
+        {
+            Console.Write("Enter miles driven in this tankful: ");
+            miles = int.Parse(Console.ReadLine());
+            mpg = (double)miles / gallons;
+            Console.WriteLine($"MPG for this tankful: {mpg:F}");
+            totalGallons += gallons;
+            totalMiles += miles;
+            Console.Write("Enter gallons in the next tankful (or number < 1 to quit): ");
+            gallons = int.Parse(Console.ReadLine());
+        }
+
+        totalMPG = (double) totalMiles / totalGallons;
+        Console.WriteLine($"Total Gallons: {totalGallons}");
+        Console.WriteLine($"Total Miles: {totalMiles}");
+        Console.WriteLine($"Total MPG: {totalMPG:F}");
+        Console.WriteLine("Thanks for checking your MPG with us today!");
+    }
+}
+
