4a15c8ab5216125da071c439e64065ccb56b196c
Danjel Nygard <danjelnygard@hotmail.com>
2018-05-31 22:04:51 -0500

    Added Exercise 4-15

---
+++ Chapter 4/Exercise 4-15/HealthProfile/HealthProfile/HealthProfile.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+class HealthProfile
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    private string gender;
+    private int birthMonth;
+    private int birthDay;
+    private int birthYear;
+    private int height;
+    private int weight;
+    private int currentYear;
+
+    public HealthProfile(string firstName, string lastName, string gender,
+        int birthMonth, int birthDay, int birthYear, int height, int weight)
+    {
+        currentYear = DateTime.Now.Year;
+        FirstName = firstName;
+        LastName = lastName;
+        Gender = gender;
+        BirthMonth = birthMonth;
+        BirthDay = birthDay;
+        BirthYear = birthYear;
+        Height = height;
+        Weight = weight;
+
+    }
+
+    public string Gender
+    {
+        get
+        {
+            return gender;
+        }
+
+        set
+        {
+            if (value == "M" || value == "m")
+            {
+                gender = value;
+            }
+
+            else if (value == "F" || value == "f")
+            {
+                gender = value;
+            }
+        }
+    }
+
+    public int BirthMonth
+    {
+        get
+        {
+            return birthMonth;
+        }
+
+        set
+        {
+            if (value > 0 && value <= 12)
+            {
+                birthMonth = value;
+            }
+        }
+
+    }
+
+    public int BirthDay
+    {
+        get
+        {
+            return birthDay;
+        }
+
+        set
+        {
+            if (value > 0 && value <= 31)
+            {
+                birthDay = value;
+            }
+        }
+
+    }
+
+    public int BirthYear
+    {
+        get
+        {
+            return birthYear;
+        }
+
+        set
+        {
+            if (value > 1900 && value < currentYear)
+            {
+                birthYear = value;
+            }
+            
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+
+        set
+        {
+            if(value > 36 && value < 96)
+            {
+                height = value;
+            }
+        }
+    }
+
+    public int Weight
+    {
+        get
+        {
+            return weight;
+        }
+
+        set
+        {
+            if (value > 0 && value < 900)
+            {
+                weight = value;
+            }
+        }
+    }
+
+    public int getAge()
+    {
+        int age = currentYear - this.BirthYear;
+        return age;
+    }
+
+    public int getMaxHeartRate()
+    {
+        return 220 - this.getAge();
+    }
+
+    public double getMinTargetRate()
+    {
+        return .5 * this.getMaxHeartRate();
+    }
+
+    public double getMaxTargetRate()
+    {
+        return .85 * this.getMaxHeartRate();
+    }
+
+    public double getBMI()
+    {
+        double BMI = (double)(Weight * 703) / (Math.Pow(height, 2));
+        return BMI;
+    }
+        
+}
+
---
+++ Chapter 4/Exercise 4-15/HealthProfile/HealthProfile/HealthProfileApp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+class HealthProfileApp
+{
+    static void Main(string[] args)
+    {
+        string firstName;
+        string lastName;
+        string gender;
+        int birthMonth;
+        int birthDay;
+        int birthYear;
+        int height;
+        int weight;
+
+        Console.Write("Enter First Name: ");
+        firstName = Console.ReadLine();
+        Console.Write("Enter Last Name: ");
+        lastName = Console.ReadLine();
+        Console.Write("Enter gender (M or F): ");
+        gender = Console.ReadLine();
+        Console.Write("Enter Birth Month (1-12): ");
+        birthMonth = int.Parse(Console.ReadLine());
+        Console.Write("Enter Birth Day (1-31): ");
+        birthDay = int.Parse(Console.ReadLine());
+        Console.Write("Enter Birth Year: ");
+        birthYear = int.Parse(Console.ReadLine());
+        Console.Write("Enter Height (in inches): ");
+        height = int.Parse(Console.ReadLine());
+        Console.Write("Enter Weight (in pounds): ");
+        weight = int.Parse(Console.ReadLine());
+
+        HealthProfile healthProfile = new HealthProfile(firstName, lastName, gender, birthMonth,
+            birthDay, birthYear, height, weight);
+
+        Console.WriteLine($"\nHello, {healthProfile.FirstName} {healthProfile.LastName}");
+        Console.WriteLine($"Gender: {healthProfile.Gender}");
+        Console.WriteLine($"Date of Birth: {healthProfile.BirthMonth}/{healthProfile.BirthDay}/{healthProfile.BirthYear}");
+        Console.WriteLine($"Height: {healthProfile.Height}");
+        Console.WriteLine($"Weight: {healthProfile.Weight}");
+        Console.WriteLine($"Age: {healthProfile.getAge()}");
+        Console.WriteLine($"Maximum Heart Rate: {healthProfile.getMaxHeartRate()}");
+        Console.WriteLine($"Target Heart Rate Range: {healthProfile.getMinTargetRate()}/{healthProfile.getMaxTargetRate()}");
+        Console.WriteLine($"BMI: {healthProfile.getBMI():F}\n");
+
+        Console.WriteLine("BMI Values");
+        Console.WriteLine("Underweight:\tless than 18.5");
+        Console.WriteLine("Normal:\t\tbetween 18.5 and 24.9");
+        Console.WriteLine("Overweight:\tbetween 25 and 29.9");
+        Console.WriteLine("Obese:\t\t30 or greater\n");
+
+    }
+}
+
