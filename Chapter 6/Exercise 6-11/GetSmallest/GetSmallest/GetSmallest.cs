cd209968b996b505ab38260719395aa3f933ccc8
Danjel Nygard <danjelnygard@hotmail.com>
2018-06-05 21:39:55 -0500

    Added Exercise 6-11

---
+++ Chapter 6/Exercise 6-11/GetSmallest/GetSmallest/GetSmallest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class GetSmallest
+{
+    static void Main(string[] args)
+    {
+        int total, value;
+        int smallest = int.MaxValue;
+
+        Console.Write("Enter the number of integers to compare: ");
+        total = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < total; ++i)
+        {
+            Console.Write("\nEnter an integer: ");
+            value = int.Parse(Console.ReadLine());
+            if (value < smallest){
+                smallest = value;
+            }
+
+        }
+
+        Console.WriteLine($"Smallest value entered: {smallest}");
+
+    }
+}
+
