29438c95290c7f458844745856c78f1cee9f9ba4
Danjel Nygard <danjelnygard@hotmail.com>
2018-06-06 21:37:56 -0500

    Added Exercise 3-25

---
+++ Chapter 3/Exercise 3-25/MultipleChecker/MultipleChecker/MultipleChecker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class MultipleChecker
+{
+    static void Main(string[] args)
+    {
+        int first, second;
+        Console.WriteLine("Multiple Checker");
+        Console.Write("Enter a number: ");
+        first = int.Parse(Console.ReadLine());
+        Console.Write("Enter another number: ");
+        second = int.Parse(Console.ReadLine());
+
+        if (first % second == 0)
+        {
+            Console.WriteLine($"{first} is a multiple of {second}");
+        }
+        else
+        {
+            Console.WriteLine($"{first} is not a multiple of {second}");
+        }
+    }
+}
+
