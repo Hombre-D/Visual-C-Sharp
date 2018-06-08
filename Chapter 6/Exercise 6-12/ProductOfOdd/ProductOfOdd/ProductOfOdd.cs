54d391e06b84381c3513d9d27d41802679653d9d
Danjel Nygard <danjelnygard@hotmail.com>
2018-06-07 21:36:49 -0500

    Added Exercise 6-12

---
+++ Chapter 6/Exercise 6-12/ProductOfOdd/ProductOfOdd/ProductOfOdd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+    class ProductOfOdd
+{
+    static void Main(string[] args)
+    {
+        int total = 1;
+
+        for (int i = 1; i <= 7; ++i)
+        {
+            if (i % 2 != 0)
+            {
+                total *= i;
+            }
+        }
+
+        Console.WriteLine($"Product of odd numbers 1-7: {total}");
+
+    }
+}
+
