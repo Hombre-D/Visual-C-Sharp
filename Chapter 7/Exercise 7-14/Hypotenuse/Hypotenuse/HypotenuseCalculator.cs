21a73d3c1a3045c22ef0cb9802d1235d1e41132e
Danjel Nygard <danjelnygard@hotmail.com>
2018-06-14 21:31:20 -0500

    Added Exercise 7-14

---
+++ Chapter 7/Exercise 7-14/Hypotenuse/Hypotenuse/HypotenuseCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class HypotenuseCalculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the hypotenuse calculator");
+        Console.Write("Enter the length of side A: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Enter the length of side B: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine($"\nThe length of the hypotenuse is: {Hypotenuse(a, b):F}");
+
+    }
+
+    static double Hypotenuse(double a, double b) => 
+        Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+}
+
