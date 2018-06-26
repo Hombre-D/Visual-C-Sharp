56e8ed6e2044cfff2c120a355ba8333f81576c1b
Danjel Nygard <danjelnygard@hotmail.com>
2018-06-25 22:29:35 -0500

    Added 3-27 and 7-21

---
+++ Chapter 3/Exercise 3-27/CharConverter/CharConverter/CharConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class CharConverter
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Char Converter!");
+        char input = ' ';
+        
+        while (input != '*')
+        {
+            try
+            {
+                Console.Write("Enter a char value (or * to quit): ");
+                input = Char.Parse(Console.ReadLine());
+                if (input == '*')
+                {
+                    Console.WriteLine("Thanks for using Char Converter!");
+                    break;
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Enter a single character.");
+                continue;
+            }
+            Console.WriteLine($"The character {input} has the int value {(int)input}.");
+        }
+    }
+}
+
---
+++ Chapter 7/Exercise 7-21/DigitSeparator/DigitSeparator/DigitSeparator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+class DigitSeparator
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter an integer between 1 and 99999: ");
+        int input = int.Parse(Console.ReadLine());
+        DisplayDigits(input);
+        
+    }
+
+    static void DisplayDigits(int a)
+    {
+        bool firstDigit = false;
+        for (int i = 10000; i >= 1;)
+        {
+            int digit = a / i;
+            int remainder = a % i;
+            i = i / 10;
+            if(digit == 0 && firstDigit == false)
+            {
+
+            }
+            else
+            {
+                Console.Write($"{digit}  ");
+                firstDigit = true;
+            }
+            
+            a = remainder;
+        }
+        Console.WriteLine("");
+    }
+
+}
+
