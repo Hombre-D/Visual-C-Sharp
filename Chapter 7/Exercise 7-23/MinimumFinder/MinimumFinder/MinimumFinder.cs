0111e0666b27ed5c4887d4cc6ca9a65802cf2bfa
Danjel Nygard <danjelnygard@hotmail.com>
2018-06-26 22:09:42 -0500

    Added 7-23 and 7-25

---
+++ Chapter 7/Exercise 7-23/MinimumFinder/MinimumFinder/MinimumFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class MinimumFinder
+{
+    static void Main(string[] args)
+    {
+        float a, b, c;
+        Console.WriteLine("Welcome to Minimum Finder!");
+        Console.Write("Enter a floating point number: ");
+        a = float.Parse(Console.ReadLine());
+        Console.Write("Enter a floating point number: ");
+        b = float.Parse(Console.ReadLine());
+        Console.Write("Enter a floating point number: ");
+        c = float.Parse(Console.ReadLine());
+
+        Console.WriteLine($"The smallest value is {Minimum(a, b, c)}");
+
+    }
+
+    static float Minimum (float a, float b, float c)
+    {
+        return Math.Min(Math.Min(a, b), c);
+    }
+
+}
+
---
+++ Chapter 7/Exercise 7-25/PrimeTest/PrimeTest/PrimeTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+class PrimeTest
+{
+    static void Main(string[] args)
+    {
+        int a;
+        Console.WriteLine("Welcome to Prime Finder!");
+        Console.Write("Enter an integer: ");
+        a = int.Parse(Console.ReadLine());
+        if (IsPrime(a))
+        {
+            Console.WriteLine("Number is prime!");
+        }
+        else
+        {
+            Console.WriteLine("Number is not prime!");
+        }
+    }
+
+    static bool IsPrime(int a)
+    {
+        if (a <= 1) return false;
+        if (a == 2) return true;
+        if (a % 2 == 0) return false;
+
+        var boundary = (int)Math.Floor(Math.Sqrt(a));
+
+        for(int i = 3; i < boundary; i += 2)
+        {
+            if (a % i == 0) return false;
+        }
+
+        return true;
+    }
+}
+
