9a371ee8aa1c6b82817b5dcd8e684872e2c4b434
Danjel Nygard <danjelnygard@hotmail.com>
2018-06-29 22:06:26 -0500

    Exercise 5-19

---
+++ Chapter 5/Exercise 5-19/WeeklySales/WeeklySales/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal totalSales = 0.0m;
+        decimal weeklyPay = 0.0m;
+
+        int item1, item2, item3, item4;
+
+        Console.Write("Enter # sold for Item 1: ");
+        item1 = int.Parse(Console.ReadLine());
+        totalSales += item1 * 239.99m;
+        Console.Write("Enter # sold for Item 2: ");
+        item2 = int.Parse(Console.ReadLine());
+        totalSales += item2 * 129.75m;
+        Console.Write("Enter # sold for Item 3: ");
+        item3 = int.Parse(Console.ReadLine());
+        totalSales += item3 * 99.95m;
+        Console.Write("Enter # sold for Item 4: ");
+        item4 = int.Parse(Console.ReadLine());
+        totalSales += item4 * 350.89m;
+
+        weeklyPay = 200 + totalSales * .09m;
+
+        Console.WriteLine($"Total Sales: {totalSales:C}");
+        Console.WriteLine($"Weekly Pay: {weeklyPay:C}");
+
+
+
+
+    }
+}
+
