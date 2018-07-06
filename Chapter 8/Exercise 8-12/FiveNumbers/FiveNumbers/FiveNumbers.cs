f1c8b21b811e5281b068f02fa2c9b90432e7ff5b
Danjel Nygard <danjelnygard@hotmail.com>
2018-07-06 00:04:25 -0500

    Added 8-12

---
+++ Chapter 8/Exercise 8-12/FiveNumbers/FiveNumbers/FiveNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+class FiveNumbers
+{
+    static void Main(string[] args)
+    {
+        var values = new int[5];
+        int counter = 0;
+
+        for (int i = 0; i <= 4; ++i)
+        {
+            int input;
+            bool isDuplicate = false;
+
+            do
+            {
+                Console.Write("Enter an integer between 10 and 100: ");
+                input = int.Parse(Console.ReadLine());
+            } while (input < 10 || input > 100 );
+
+            for (int j = 0; j <= i; j++)
+            {
+                if (input == values[j]) {
+                    Console.WriteLine("Duplicate Input");
+                    isDuplicate = true;
+                }
+                
+            }
+
+            
+            if (isDuplicate == false)
+            {   
+                values[counter] = input;
+                ++counter;
+            }
+
+            Console.WriteLine($"Numbers Entered: {i + 1}");
+            for (int k = 0; k < counter; ++k)
+                Console.Write($"{values[k]} ");
+            Console.WriteLine();
+
+        }
+
+    }
+}
+
