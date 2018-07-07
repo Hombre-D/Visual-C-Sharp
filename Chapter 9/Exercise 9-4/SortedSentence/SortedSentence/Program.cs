ca55955babb274c871c2c6154c0436965436be16
Danjel Nygard <danjelnygard@hotmail.com>
2018-07-06 21:40:14 -0500

    Added 9-4

---
+++ Chapter 9/Exercise 9-4/SortedSentence/SortedSentence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sentence;
+
+        Console.WriteLine("Welcome to Sentence Sorter!");
+        Console.Write("Enter a sentence: ");
+        sentence = Console.ReadLine();
+        string [] splitSentence = sentence.ToLower().Split();
+
+        var sortedSentence =
+            from word in splitSentence
+            orderby word
+            select word;
+
+        foreach (var word in sortedSentence.Distinct())
+        {
+            Console.WriteLine(word);
+        }
+
+
+
+    }
+}
+
