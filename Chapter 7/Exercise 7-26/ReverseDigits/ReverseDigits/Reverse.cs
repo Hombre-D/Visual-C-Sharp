b8a377341970ae7d7235cc7f2e1f8808b5691d54
Danjel Nygard <danjelnygard@hotmail.com>
2018-07-08 22:09:59 -0500

    Added 7-26

---
+++ Chapter 7/Exercise 7-26/ReverseDigits/ReverseDigits/Reverse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+class Reverse
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Digit Reverser!");
+        Console.Write("Enter an integer: ");
+        int input = int.Parse(Console.ReadLine());
+        int reverse = Reversed(input);
+        Console.WriteLine($"Reversed integer: {reverse}");
+
+    }
+
+    static int Reversed(int a)
+    {
+        int quotient = a / 10;
+        int remainder = a % 10;
+        int reverse = remainder;
+        while(quotient != 0)
+        {
+            reverse *= 10;
+            remainder = quotient % 10;
+            quotient /= 10;
+            reverse += remainder;
+        }
+
+        return reverse;
+
+    }
+
+}
+
