760d61f947de6573394903cf5d8b9c799ed4c8f7
Danjel Nygard <danjelnygard@hotmail.com>
2018-07-09 21:46:31 -0500

    Added 10-3

---
+++ Chapter 10/Exercise 10-3/Rectangle/Rectangle/Rectangle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Rectangle
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length = 1, double width = 1)
+    {
+        Length = length;
+        Width = width;
+    }
+
+
+    public double Length
+    {
+        get
+        {
+            return length;
+        }
+
+        set
+        {
+            if(value < 0.0 || value > 20.0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Length), value,
+                    $"{nameof(Length)} must be between 0.0 and 20.0");
+            }
+
+            length = value;
+
+        }
+
+    }
+
+    public double Width
+    {
+        get
+        {
+            return width;
+        }
+
+        set
+        {
+            if (value < 0.0 || value > 20.0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Width), value,
+                    $"{nameof(Width)} must be between 0.0 and 20.0");
+            }
+
+            width = value;
+
+        }
+    }
+
+    public double Perimeter
+    {
+        get
+        {
+            return 2 * (Length + Width);
+        }
+    }
+
+    public double Area
+    {
+        get
+        {
+            return Length * Width;
+        }
+    }
+
+    public override string ToString() =>
+        $"Length: {Length}, Width: {Width}, Perimeter: {Perimeter}, Area: {Area}";
+    
+}
+
---
+++ Chapter 10/Exercise 10-3/Rectangle/Rectangle/RectangleTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class RectangleTest
+{
+    static void Main(string[] args)
+    {
+        Rectangle rectangle1 = new Rectangle(2.0, 4.5);
+        Rectangle rectangle2 = new Rectangle(19.9, 15.4);
+
+        try
+        {
+            Rectangle rectangle3 = new Rectangle(5.0, -5.5);
+        }
+
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine(rectangle1);
+        Console.WriteLine(rectangle2);
+    }
+}
+
