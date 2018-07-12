662efdc9da05411f91171fcfdcfc9a7048cb37e0
Danjel Nygard <danjelnygard@hotmail.com>
2018-07-11 21:44:18 -0500

    Added 10-5

---
+++ Chapter 10/Exercise 10-5/SavingsAcct/SavingsAcct/SavingsAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class SavingsAccount
+{
+    public static decimal AnnualInterestRate { get; private set; }
+    public decimal SavingsBalance { get; set; }
+
+    public SavingsAccount(decimal savings)
+    {
+        SavingsBalance = savings;
+    }
+
+    public void CalculateMonthlyInterest()
+    {
+        decimal interest = this.SavingsBalance * AnnualInterestRate / 12;
+        this.SavingsBalance += interest;
+    }
+
+    public static void SetInterestRate(decimal rate)
+    {
+        AnnualInterestRate = rate;
+    }
+
+}
+
---
+++ Chapter 10/Exercise 10-5/SavingsAcct/SavingsAcct/SavingsAccountTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+class SavingsAccountTest
+{
+    static void Main(string[] args)
+    {
+        SavingsAccount saver1 = new SavingsAccount(2000.0m);
+        SavingsAccount saver2 = new SavingsAccount(3000.0m);
+
+        SavingsAccount.SetInterestRate(.04m);
+
+        Console.WriteLine($"Saver 1 initial balance: {saver1.SavingsBalance:C}");
+        Console.WriteLine($"Saver 2 initial balance: {saver2.SavingsBalance:C}");
+
+        saver1.CalculateMonthlyInterest();
+        saver2.CalculateMonthlyInterest();
+
+        Console.WriteLine($"Saver 1 first new balance: {saver1.SavingsBalance:C}");
+        Console.WriteLine($"Saver 2 first new balance: {saver2.SavingsBalance:C}");
+
+        SavingsAccount.SetInterestRate(.05m);
+
+        saver1.CalculateMonthlyInterest();
+        saver2.CalculateMonthlyInterest();
+
+        Console.WriteLine($"Saver 1 second new balance: {saver1.SavingsBalance:C}");
+        Console.WriteLine($"Saver 2 second new balance: {saver2.SavingsBalance:C}");
+    }
+}
+
