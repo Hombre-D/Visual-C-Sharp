01ac504ecdfea6b9c85199645439c8157e181c1d
Danjel Nygard <danjelnygard@hotmail.com>
2018-07-24 22:32:16 -0500

    Began 14-11

---
+++ Chapter 14/Exercise 14-11/VirtualKeyboard/VirtualKeyboard/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirtualKeyboard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Q)
+            {
+                buttonQ.BackColor = Color.Black;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Q)
+            {
+                buttonQ.UseVisualStyleBackColor = true;
+                buttonQ.BackColor = default(Color);
+            }
+        }
+    }
+}
