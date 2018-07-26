e3b0f397dd0c6f6e093ac3046d145e2eb2a05896
Danjel Nygard <danjelnygard@hotmail.com>
2018-07-25 22:48:47 -0500

    14-11 updated with dictionary

--- Chapter 14/Exercise 14-11/VirtualKeyboard/VirtualKeyboard/Form1.Designer.cs
+++ Chapter 14/Exercise 14-11/VirtualKeyboard/VirtualKeyboard/Form1.Designer.cs
@@ -29,6 +29,19 @@
         private void InitializeComponent()
         {
             this.buttonQ = new System.Windows.Forms.Button();
+            this.buttonW = new System.Windows.Forms.Button();
+            this.buttonE = new System.Windows.Forms.Button();
+            this.buttonR = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.buttonP = new System.Windows.Forms.Button();
+            this.buttonO = new System.Windows.Forms.Button();
+            this.buttonI = new System.Windows.Forms.Button();
+            this.buttonU = new System.Windows.Forms.Button();
+            this.buttonY = new System.Windows.Forms.Button();
+            this.buttonT = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // buttonQ
@@ -41,11 +54,139 @@
             this.buttonQ.Text = "Q";
             this.buttonQ.UseVisualStyleBackColor = false;
             // 
+            // buttonW
+            // 
+            this.buttonW.Location = new System.Drawing.Point(91, 77);
+            this.buttonW.Name = "buttonW";
+            this.buttonW.Size = new System.Drawing.Size(43, 38);
+            this.buttonW.TabIndex = 1;
+            this.buttonW.Text = "W";
+            this.buttonW.UseVisualStyleBackColor = true;
+            // 
+            // buttonE
+            // 
+            this.buttonE.Location = new System.Drawing.Point(138, 77);
+            this.buttonE.Name = "buttonE";
+            this.buttonE.Size = new System.Drawing.Size(43, 38);
+            this.buttonE.TabIndex = 2;
+            this.buttonE.Text = "E";
+            this.buttonE.UseVisualStyleBackColor = true;
+            // 
+            // buttonR
+            // 
+            this.buttonR.Location = new System.Drawing.Point(186, 77);
+            this.buttonR.Name = "buttonR";
+            this.buttonR.Size = new System.Drawing.Size(43, 38);
+            this.buttonR.TabIndex = 3;
+            this.buttonR.Text = "R";
+            this.buttonR.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(387, 214);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(43, 38);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "W";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(395, 222);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(43, 38);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "W";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(403, 230);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(43, 38);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "W";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // buttonP
+            // 
+            this.buttonP.Location = new System.Drawing.Point(470, 77);
+            this.buttonP.Name = "buttonP";
+            this.buttonP.Size = new System.Drawing.Size(43, 38);
+            this.buttonP.TabIndex = 7;
+            this.buttonP.Text = "P";
+            this.buttonP.UseVisualStyleBackColor = true;
+            // 
+            // buttonO
+            // 
+            this.buttonO.Location = new System.Drawing.Point(423, 77);
+            this.buttonO.Name = "buttonO";
+            this.buttonO.Size = new System.Drawing.Size(43, 38);
+            this.buttonO.TabIndex = 8;
+            this.buttonO.Text = "O";
+            this.buttonO.UseVisualStyleBackColor = true;
+            // 
+            // buttonI
+            // 
+            this.buttonI.Location = new System.Drawing.Point(376, 77);
+            this.buttonI.Name = "buttonI";
+            this.buttonI.Size = new System.Drawing.Size(43, 38);
+            this.buttonI.TabIndex = 9;
+            this.buttonI.Text = "I";
+            this.buttonI.UseVisualStyleBackColor = true;
+            // 
+            // buttonU
+            // 
+            this.buttonU.Location = new System.Drawing.Point(328, 77);
+            this.buttonU.Name = "buttonU";
+            this.buttonU.Size = new System.Drawing.Size(43, 38);
+            this.buttonU.TabIndex = 10;
+            this.buttonU.Text = "U";
+            this.buttonU.UseVisualStyleBackColor = true;
+            // 
+            // buttonY
+            // 
+            this.buttonY.Location = new System.Drawing.Point(280, 77);
+            this.buttonY.Name = "buttonY";
+            this.buttonY.Size = new System.Drawing.Size(43, 38);
+            this.buttonY.TabIndex = 11;
+            this.buttonY.Text = "Y";
+            this.buttonY.UseVisualStyleBackColor = true;
+            // 
+            // buttonT
+            // 
+            this.buttonT.Location = new System.Drawing.Point(233, 77);
+            this.buttonT.Name = "buttonT";
+            this.buttonT.Size = new System.Drawing.Size(43, 38);
+            this.buttonT.TabIndex = 12;
+            this.buttonT.Text = "T";
+            this.buttonT.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 20);
+            this.textBox1.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(560, 303);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonT);
+            this.Controls.Add(this.buttonY);
+            this.Controls.Add(this.buttonU);
+            this.Controls.Add(this.buttonI);
+            this.Controls.Add(this.buttonO);
+            this.Controls.Add(this.buttonP);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonR);
+            this.Controls.Add(this.buttonE);
+            this.Controls.Add(this.buttonW);
             this.Controls.Add(this.buttonQ);
             this.KeyPreview = true;
             this.Name = "Form1";
@@ -53,12 +194,26 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonQ;
+        private System.Windows.Forms.Button buttonW;
+        private System.Windows.Forms.Button buttonE;
+        private System.Windows.Forms.Button buttonR;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button buttonP;
+        private System.Windows.Forms.Button buttonO;
+        private System.Windows.Forms.Button buttonI;
+        private System.Windows.Forms.Button buttonU;
+        private System.Windows.Forms.Button buttonY;
+        private System.Windows.Forms.Button buttonT;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Chapter 14/Exercise 14-11/VirtualKeyboard/VirtualKeyboard/Form1.cs
+++ Chapter 14/Exercise 14-11/VirtualKeyboard/VirtualKeyboard/Form1.cs
@@ -12,6 +12,8 @@ namespace VirtualKeyboard
 {
     public partial class Form1 : Form
     {
+        private string textBoxString = string.Empty;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,19 +21,45 @@ namespace VirtualKeyboard
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Q)
+            var keys = getDict();
+
+            foreach(var item in keys)
             {
-                buttonQ.BackColor = Color.Black;
+                if (item.Key == e.KeyCode)
+                {
+                    item.Value.BackColor = Color.Black;
+                    textBoxString += item.Value.Text;
+                }
             }
+            textBox1.Text = textBoxString;
         }
+
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Q)
+            var keys = getDict();
+
+            foreach (var item in keys)
             {
-                buttonQ.UseVisualStyleBackColor = true;
-                buttonQ.BackColor = default(Color);
-            }
+                if (item.Key == e.KeyCode)
+                {
+                    item.Value.UseVisualStyleBackColor = true;
+                    item.Value.BackColor = default(Color);
+                }
+            } 
+            
+        }
+
+        //This method prepares and returns a dictionary for use in KeyDown/KeyUp
+        private IDictionary<Keys, Button> getDict()
+        {
+            IDictionary<Keys, Button> keys = new Dictionary<Keys, Button>()
+                {{Keys.Q, buttonQ},{Keys.W, buttonW},{Keys.E, buttonE},{Keys.R, buttonR},
+                {Keys.T, buttonT},{Keys.Y, buttonY},{Keys.U, buttonU},{Keys.I, buttonI},
+                {Keys.O, buttonO},{Keys.P, buttonP},
+
+                };
+            return keys;
         }
     }
 }
