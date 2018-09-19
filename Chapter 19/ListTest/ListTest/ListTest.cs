c4ce9e6858348d645f082debaed2f4f306e2b30a
Danjel Nygard <danjelnygard@hotmail.com>
2018-09-18 23:27:14 -0500

    Began Chapter 19

---
+++ Chapter 19/ListTest/ListTest/ListTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using LinkedListLibrary;
+
+
+class ListTest
+{
+    static void Main()
+    {
+        var list = new List();
+        bool aBoolean = true;
+        char aCharacter = '$';
+        int anInteger = 34567;
+        string aString = "hello";
+
+        list.InsertAtFront(aBoolean);
+        list.Display();
+        list.InsertAtFront(aCharacter);
+        list.Display();
+        list.InsertAtBack(anInteger);
+        list.Display();
+        list.InsertAtBack(aString);
+        list.Display();
+
+        try
+        {
+            object removedObject = list.RemoveFromFront();
+            Console.WriteLine($"{removedObject} removed");
+            list.Display();
+
+            removedObject = list.RemoveFromFront();
+            Console.WriteLine($"{removedObject} removed");
+            list.Display();
+
+            removedObject = list.RemoveFromBack();
+            Console.WriteLine($"{removedObject} removed");
+            list.Display();
+
+            removedObject = list.RemoveFromBack();
+            Console.WriteLine($"{removedObject} removed");
+            list.Display();
+        }
+        catch(EmptyListException emptyListException)
+        {
+            Console.Error.WriteLine($"\n{emptyListException}");
+        }
+
+
+    }
+}
+
